bacadf03d6081a03c7eb0eb4197724d8034eeb98
Rami Maalouf <rami.rami@ucalgary.ca>
2022-12-26 13:31:40 -0700

    changed pos of text relative to box AGAIN

--- components/song.tsx
+++ components/song.tsx
@@ -49,7 +49,7 @@ export default function Song({ song, index }: { song: SongType; index: number })
     // ref.current.position.y = -(index * boxSeparation) + 1;
     // } else
     ref.current.position.y = -(index * boxSeparation);
-    htmlTextRef.current.position.y = -(index * boxSeparation) * (2.08);
+    htmlTextRef.current.position.y = -(index * boxSeparation) * (2.05 + (width / MyWidth) * 0.05);
 
     if (hovered) {
       ref.current.rotation.y += 0.01;
